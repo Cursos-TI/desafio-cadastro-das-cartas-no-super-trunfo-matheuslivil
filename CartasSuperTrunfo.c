d212da99ba6856e24c4a09a205ab28635ee3c5ad
MatheusDevT <matheus_faire@hotmail.com>
2025-05-01 21:27:30 +0000

    coleta dos dados com o usuario carta1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,16 +18,34 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    char Carta [30];
+    char Carta1;
     char Estado [40];
-    char Codigo [10];
+    char Codigo[10];
     char Cidade [50];
     int Populacao;
     float Area;
     float PIB;
     int Turisticos;
-    
 
+printf("Carta1\n"); //numero da carta para indentificação 
+printf("Digite o estado:\n"); //impressão da interação com o usuario para entrada de dados
+scanf("%s", &Estado);//entrada de dados para imprimir o string de char estado. 
+printf("Codigo da carta:\n");
+scanf("%s", &Codigo);
+printf("Digite a cidade:\n");
+scanf("%s",&Cidade);
+printf("Informe a populacao da cidade:\n");
+scanf("%i", &Populacao);
+printf("Informe a area:\n");
+scanf("%f", &Area);
+printf("Informe o PIB da cidade:\n");
+scanf("%f", &PIB);
+printf("Informe a quantidade de pontos turisticos\n");
+scanf("%i", &Turisticos);
+
+
+//Impressão dos dados coletados da carta1
+printf("Estado:\n %s",&Estado);
 
     return 0;
 }
