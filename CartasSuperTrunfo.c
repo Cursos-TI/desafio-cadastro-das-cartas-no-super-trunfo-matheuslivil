e9f2da14c7b6b129dc3b67f6d2aee6a563a50e99
MatheusDevT <matheus_faire@hotmail.com>
2025-05-11 20:40:21 +0000

    carta dois

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,6 +11,7 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     // Cadastro das Cartas:
+    
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
@@ -18,21 +19,21 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    char Carta1;
-    char Estado [50];
-    char Codigo[10];
-    char Cidade [50];
-    int Populacao;
-    float Area;
-    float PIB;
-    int Turisticos;
+    char Carta1, Carta2;
+    char Estado, Estado2 ;
+    char Codigo, Codigo2;
+    char Cidade, Cidade2;
+    int Populacao, Populacao2;
+    float Area, Area2;
+    float PIB, PIB2;
+    int Turisticos, Turisticos2;
 
 printf("Carta1\n"); //numero da carta para indentificação 
 printf("Digite o estado:\n"); //impressão da interação com o usuario para entrada de dados
-fgets(Estado, 50, stdin);// acrescenta espaçp 
+fgets(Estado, 50, stdin);// foi utilizado o tipo de dados fgets para acrescentar a funcão de espaco na entrada de dados 
      
 printf("Codigo da carta:\n");
-scanf("%s", &Codigo);
+scanf("%c", &Codigo);
 printf("Digite a cidade:\n");
 scanf("%s", &Cidade);
 printf("Informe a populacao da cidade:\n");
@@ -55,8 +56,33 @@ printf("Area:%f\n", &Area);
 printf("PIB:%f\n", &PIB);
 printf("Pontos Turisticos:%i\n", &Turisticos);
 
+printf("Carta2\n"); //numero da carta para indentificação 
+printf("Digite o estado:\n"); //impressão da interação com o usuario para entrada de dados
+fgets(Estado2, 50, stdin);// foi utilizado o tipo de dados fgets para acrescentar a funcão de espaco na entrada de dados 
+     
+printf("Codigo da carta:\n");
+scanf("%c", &Codigo2);
+printf("Digite a cidade:\n");
+scanf("%s", &Cidade2);
+printf("Informe a populacao da cidade:\n");
+scanf("%i", &Populacao2);
+printf("Informe a area:\n");
+scanf("%f", &Area2);
+printf("Informe o PIB da cidade:\n");
+scanf("%f", &PIB2);
+printf("Informe a quantidade de pontos turisticos\n");
+scanf("%i", &Turisticos2);
+printf("\n");
 
-
+/*Impressão dos dados coletados da carta1*/
+printf("Carta1\n");
+printf("Estado:%s \n",&Estado2);
+printf("Codigo:%s \n", &Codigo2);
+printf("Cidade:%s\n", &Cidade2);
+printf("Populacao:%i\n",&Populacao2);
+printf("Area:%f\n", &Area2);
+printf("PIB:%f\n", &PIB2);
+printf("Pontos Turisticos:%i\n", &Turisticos2);
 
     return 0;
 }
